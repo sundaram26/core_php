b7dc4979a67cd696b74693dab55c169898d3250c
sundaram26 <singhsundaram2004@gmail.com>
2025-06-15 00:37:16 +0530

    string case method explain

--- 07_stringMethods/index.php
+++ 07_stringMethods/index.php
@@ -2,10 +2,10 @@
     $str = "sUndaram Singh";
 
     echo $str."<br />";
-    echo strtolower($str)."<br>";
-    echo strtoupper($str)."<br>";
-    echo lcfirst($str)."<br>";
-    echo ucfirst($str)."<br>";
-    echo ucwords($str)."<br>";
+    echo strtolower($str)."<br>";    //convert the string to lowercase
+    echo strtoupper($str)."<br>";   //convert the string to uppercase
+    echo lcfirst($str)."<br>";      //convert the first letter to lowercase
+    echo ucfirst($str)."<br>";      //convert the first letter to uppercase
+    echo ucwords($str)."<br>";      //convert the all the letter to uppercase
 
 ?>
